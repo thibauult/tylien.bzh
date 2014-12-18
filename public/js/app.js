37a16e007225f56c3a57a51baf3f09981704e7e9
tibus29 <thibault.pensec@gmail.com>
2014-12-18 14:32:18 +0100

    design and form controls

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,93 @@
 /**
  * Created by tibus on 16/12/14.
  */
-var tyLienApp = angular.module('tyLienApp', [])
+var tyLienApp = angular.module('tyLienApp', ['pascalprecht.translate'])
 
-    .controller('HomeCtrl', function ($scope, $http, $location) {
+    .config(function($translateProvider) {
 
-        $scope.url = 'www.google.com';
+        $translateProvider.translations('en', {
+            'title.app' : 'TyLien.bzh',
+            'msg.url' : 'Paste your long URL here',
+            'btn.shorten' : 'Shorten',
+
+            'title.shortened.link' : 'Your shortened link'
+        });
+
+        $translateProvider.translations('fr', {
+            'title.app' : 'TyLien.bzh',
+            'msg.url' : 'Collez ici votre lien',
+            'btn.shorten' : 'Valider',
+
+            'title.shortened.link' : 'Votre ty lien'
+        });
+
+        $translateProvider.translations('bzh', {
+            'title.app' : 'TyLien.bzh',
+            'msg.url' : 'Colle ton lien ici que c\'est',
+            'btn.shorten' : 'Kenavo !',
+
+            'title.shortened.link' : 'Voila ton lien mignon'
+        });
+
+        $translateProvider.preferredLanguage('fr');
+
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    })
+
+    .controller('HeaderCtrl', function($scope, $translate) {
+
+        $scope.currentLang = $translate.preferredLanguage();
+
+        $scope.setLang = function(key) {
+            $translate.use(key);
+            $scope.currentLang = key;
+        };
+    })
+
+    .controller('HomeCtrl', function ($rootScope, $scope, $http, $location, $translate) {
+
+        var urlPattern = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+
+        $scope.url = '';
+        $scope.urlPlaceholder = '';
+
         $scope.result = {
             host: $location.host(),
             port: $location.port()
         };
 
         $scope.submit = function() {
-            $http.post('/', { url: $scope.url })
-                .success(function(data) {
-                    console.log(data);
 
-                    $scope.result.key = data._id;
-                });
+            var url = $scope.url;
+
+            if(url) {
+
+                url = url.indexOf('www.') == 0 ? 'http://' + url : url;
+
+                if(urlPattern.test(url)) {
+
+                    $http.post('/', { url: url })
+                        .success(function(data) {
+                            $scope.result.key = data._id;
+                            $scope.url = url;
+                        }
+                    );
+                }
+            }
         }
+
+        $translate('msg.url').then(function(translation) {
+            $scope.urlPlaceholder = translation;
+        });
+
+        $rootScope.$on('$translateChangeSuccess', function (data) {
+
+            $translate('msg.url').then(function (translation) {
+                $scope.urlPlaceholder = translation;
+            });
+        });
 
     });
 
